2cca38361409930dc8e4faed97b79a33066b4d08
Erico Moura <ericomoura96@gmail.com>
2019-10-05 20:56:41 -0300

    Implementada medidas de performance para datasets multiclasse

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@
 from data import Data
 from decisionTree import DecisionNode, DecisionTree, RandomForest
 
-def evaluatePerformance(data):
+def evaluatePerformance(data, nForests=3):
     #Splits instances into folds
-    folds = data.generateStratifiedFolds(3)
+    folds = data.generateStratifiedFolds(nForests)
 
     allPerformances = []
     allPrecisions = []
@@ -32,47 +32,52 @@ def evaluatePerformance(data):
         for instance in forest.testingData.instances:
             predictions.append(forest.classify(instance))
 
-
         #Analizes results
-        positiveClasses = ["Sim"]   #MUST BE SET MANUALLY FOR EACH DATASET
-        negativeClasses = ["Nao"]   #MUST BE SET MANUALLY FOR EACH DATASET
-        truePositives = 0
-        falsePositives = 0
-        trueNegatives = 0
-        falseNegatives = 0
-        for i in range(len(predictions)):
-            if predictions[i] in positiveClasses:   #Predicted positive
-                if forest.testingData.instances[i][forest.testingData.className] in positiveClasses:    #Supposed to be positive
-                    truePositives += 1
-                if forest.testingData.instances[i][forest.testingData.className] in negativeClasses:    #Supposed to be negative
-                    falsePositives +=1
-            if predictions[i] in negativeClasses:   #Predicted negative
-                if forest.testingData.instances[i][forest.testingData.className] in negativeClasses:    #Supposed to be negative
-                    trueNegatives +=1
-                if forest.testingData.instances[i][forest.testingData.className] in positiveClasses:    #Supposed to be positive
-                    falseNegatives += 1
+        iterationPerformances = []
+        iterationPrecisions = []
+        iterationRecalls = []
+        #Calculates performance, recall and precision for every class
+        for classValue in forest.testingData.listClassValues():
+            positiveClass = classValue
 
-        performance = (truePositives+trueNegatives)/len(predictions)    #Right guesses
-        precision = truePositives / (truePositives + falsePositives)    #Right guesses from instances guessed positive
-        recall = truePositives / (truePositives + falseNegatives)   #Right guesses from instances that were supposed to be positive
+            truePositives = 0
+            falsePositives = 0
+            trueNegatives = 0
+            falseNegatives = 0
+            for i in range(len(predictions)):
+                if predictions[i] == positiveClass:   #Predicted positive
+                    if forest.testingData.instances[i][forest.testingData.className] == positiveClass:    #Supposed to be positive
+                        truePositives += 1
+                    else:    #Supposed to be negative
+                        falsePositives +=1
+                else:   #Predicted negative
+                    if forest.testingData.instances[i][forest.testingData.className] != positiveClass:    #Supposed to be negative
+                        trueNegatives +=1
+                    else:    #Supposed to be positive
+                        falseNegatives += 1
 
+            iterationPerformances.append((truePositives+trueNegatives)/len(predictions))    #Right guesses
+            iterationPrecisions.append(truePositives / (truePositives + falsePositives))    #Right guesses from instances guessed positive
+            iterationRecalls.append(truePositives / (truePositives + falseNegatives))   #Right guesses from instances that were supposed to be positive
+
+        #Calculates average performance, recall and precision for this iteration
+        iterationAvgPerformance = sum(iterationPerformances)/len(iterationPerformances)
+        iterationAvgPrecision = sum(iterationPrecisions)/len(iterationPrecisions)
+        iterationAvgRecall = sum(iterationRecalls)/len(iterationRecalls)
 
         print("----- Forest {} -----".format(iteration))
-        print(predictions)
-        for instance in forest.testingData.instances:
-            print(instance)
-        print("TP: {}, FP: {}, TN: {}, FN: {}".format(truePositives, falsePositives, trueNegatives, falseNegatives))
         forest.evaluateTreesPerformance()
-        print("Forest performance: {:.2f}% of guesses (precision: {:.2f}% / recall: {:.2f}%)".format(performance*100, precision*100, recall*100))
+        print("Forest performance: {:.2f}% of guesses (precision: {:.2f}% / recall: {:.2f}%)".format(iterationAvgPerformance*100, iterationAvgPrecision*100, iterationAvgRecall*100))
 
-        allPerformances.append(performance)   #Adds this iteration's performance to the list
-        allPrecisions.append(precision)   #Adds this iteration's precision to the list
-        allRecalls.append(recall)   #Adds this iteration's recall to the list
+        allPerformances.append(iterationAvgPerformance)   #Adds this iteration's performance to the list
+        allPrecisions.append(iterationAvgPrecision)   #Adds this iteration's precision to the list
+        allRecalls.append(iterationAvgRecall)   #Adds this iteration's recall to the list
 
+    #Calculates averages for all iterations
     avgPerformance = sum(allPerformances)/len(allPerformances)
     avgPrecision = sum(allPrecisions)/len(allPrecisions)
     avgRecall = sum(allRecalls)/len(allRecalls)
-
+    #Calculates F1-Measure for all iterations
     f1 = (2*avgPrecision*avgRecall) / (avgPrecision+avgRecall)
 
     print("----------")
